912fedb17d15c44253b19181269820dd9f6fbad5
OriDaer <d47709740@alumnos.itr.edu.ar>
2025-04-02 14:53:49 -0300

    fin

--- simulac.py
+++ simulac.py
@@ -1,29 +1,21 @@
 class Nodo:
-    def __init__(self, nombre):
+    def __init__(self, nombre):#Inicializa un nodo con un nombre y una lista vacía de conexiones.
         self.nombre = nombre
         self.conexiones = []
-
-    def agregar_conexion(self, nodo):
+    def agregar_conexion(self, nodo):#Agrega una conexión entre el nodo actual y otro nodo.
         self.conexiones.append(nodo)
-
-    def enviar_mensaje(self, mensaje):
+    def enviar_mensaje(self, mensaje):#Simula el envío de un mensaje desde el nodo actual a todos sus nodos conectados.
+        print(f"{self.nombre} está enviando el mensaje '{mensaje}' a sus conexiones.")
         for conexion in self.conexiones:
-            conexion.recibir_mensaje(mensaje, self)
-
-    def recibir_mensaje(self, mensaje, remitente):
-        print(f"{self.nombre} recibió: '{mensaje}' de {remitente.nombre}")
-
-# creo los nodos
+            conexion.recibir_mensaje(mensaje, self.nombre)
+    def recibir_mensaje(self, mensaje, remitente):#Simula la recepción de un mensaje desde otro nodo
+        print(f"{self.nombre} ha recibido el mensaje '{mensaje}' de {remitente}")
 servidor = Nodo("Servidor")
-cliente1 = Nodo("Cliente1")
-cliente2 = Nodo("Cliente2")
-cliente3 = Nodo("Cliente3")
-
-# hago las conexiones entre ellos
+cliente1 = Nodo("Cliente 1")
+cliente2 = Nodo("Cliente 2")
+cliente3 = Nodo("Cliente 3")
 servidor.agregar_conexion(cliente1)
-cliente1.agregar_conexion(servidor)
 servidor.agregar_conexion(cliente2)
-cliente2.agregar_conexion(servidor)
 servidor.agregar_conexion(cliente3)
-cliente3.agregar_conexion(servidor)
-servidor.enviar_mensaje("Mensaje de prueba para todos los clientes")
+mensaje = "Hola, clientes!"
+servidor.enviar_mensaje(mensaje)
